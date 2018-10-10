e4161372a9197df74efdb1220344f3e09eb31ca1
matthew seyer <matthew.seyer@gmail.com>
2018-10-09 23:27:33 -0600

    updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='winobjid',
-    version="0.0.1",
+    version="0.0.2",
     description='Windows Tools and Lib for parsing Object IDs to JSONL.',
     author='Matthew Seyer',
     url='https://github.com/forensicmatt/WinObjectIdParser',
